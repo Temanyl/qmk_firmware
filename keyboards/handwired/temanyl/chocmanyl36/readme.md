c4c9136997cc3dba211fb7aa088dd93803e4016b
Benjamin Bortfeldt-Roß <bbt@miamed.de>
2023-07-22 23:47:34 +0200

    add configs for fulcrum34 and chocmanyl36

---
+++ keyboards/handwired/temanyl/chocmanyl36/readme.md
@@ -0,0 +1,4 @@
+# Chromanyl36
+
+Coming soon
+
--- keyboards/handwired/temanyl/fulcrum34/keymaps/temanyl/keymap.c
+++ keyboards/handwired/temanyl/fulcrum34/keymaps/temanyl/keymap.c
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include <SPI.h>
 
 #include <stdio.h>
 char wpm_str[10];
